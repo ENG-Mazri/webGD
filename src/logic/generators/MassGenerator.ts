2615e0ef4724a06cb39575ad0d9883a79d0ac618
Mazri A <eng.mazri@gmail.com>
2023-06-01 14:19:32 +0300

    add new way to genearate mass

--- src/logic/generators/MassGenerator.ts
+++ src/logic/generators/MassGenerator.ts
@@ -324,3 +324,173 @@ export class MassGenerator extends Generator {
 
 
 // TODO: Extusion
+
+//TODO: NEW WAY Site - building contour - check for intersection with site boudries
+// //* Contour
+// const mat = new THREE.LineBasicMaterial( {color: 0x6600cc, linewidth: 2} );
+// const contourLines = [];
+// const contourLines3 = []
+
+// for( let i=0; i < contour1.length - 1; i++ ){
+//   const geometry = new THREE.BufferGeometry().setFromPoints([contour1[i],contour1[i+1]]);
+//   const line = new THREE.Line(geometry, mat);
+//   line.rotation.set(Math.PI/2,0, 0);
+//   line.updateMatrix()
+
+//   let l3 = new THREE.Line3(new THREE.Vector3(contour1[i].x, 0, contour1[i].y),
+//                             new THREE.Vector3(contour1[i+1].x, 0, contour1[i+1].y));
+//   contourLines.push(line)
+//   contourLines3.push(l3)
+//   scene.add(line)
+// }
+
+
+// //* get bbox of contour
+// const shape = formBaseShape(contour1);
+// const geom = new THREE.ExtrudeGeometry( shape, extrudeSettings ); // try using buffergeometry instead
+// let meShape = new THREE.Mesh( geom, mat );
+// meShape.rotation.set(Math.PI/2,0, 0);
+// meShape.updateMatrix();
+// meShape.geometry.computeBoundingBox();
+// const bbox = visualizeBBox( meShape.geometry.boundingBox, false );
+// const dist = bbox.min.distanceTo(bbox.max) / 1.7;
+// const axesLines = drawAxes(bbox, false);
+
+// //* Generate podium base position inside site
+
+// const podiumBaseGeom = new THREE.BoxGeometry( dist, dist/2, 0.1 ); 
+// const podiumBaseMat = new THREE.MeshBasicMaterial( {color: 0x66ffcc, side: THREE.DoubleSide} );
+// const podiumBase = new THREE.Mesh( podiumBaseGeom, podiumBaseMat );
+// let times=0
+// let intersections = [];
+// let rotation = 0;
+// podiumBase.geometry.computeBoundingBox()
+// podiumBase.updateMatrixWorld()
+// scene.add( podiumBase );
+
+// while(intersections.includes(true) || intersections.length == 0) {
+
+//   let n = Math.random();
+//   let axis = randomIntFromInterval(0,axesLines.length-1)
+//   let pointAtParam = getPointAtParameter(axesLines[axis], n, false);
+
+//   podiumBase.rotation.set(Math.PI/2, 0, rotation);
+//   podiumBase.position.copy(pointAtParam)
+//   podiumBase.updateMatrix()
+
+//   for ( let j = 0; j < contourLines.length; j++)
+//     intersections.push(checkTwoShapeIntersect(podiumBase, contourLines[j]));
+
+//   // console.log(intersections)
+
+//   if(intersections.includes(true)) {
+//     intersections = []
+//     rotation = Math.PI / (Math.random() * 4)
+//     times++;
+//     if(times == 500) break;
+//   } 
+// }
+// console.log(`Found base position and orientation after: ${times} times`)
+// podiumBase.visible = false;
+
+// let topPodium = 0;
+
+// function createPodium(width, height, depth, matrix, nFloor, floorHeight){
+//   const geomF = new THREE.BoxGeometry( width, height, depth ); 
+//   const matF = new THREE.MeshPhongMaterial( {color: 0xd9d9d9, side: THREE.DoubleSide} );
+  
+//   const geomS = new THREE.BoxGeometry( width, height, floorHeight ); 
+//   const matS = new THREE.MeshPhongMaterial( {color: 0xb3e6ff, side: THREE.DoubleSide, transparent: true, opacity: 0.7} );
+//   let hF = depth;
+//   let hS = (floorHeight + hF)/2 + hF;
+
+//   const floorMeshes = []
+//   const spaceMeshes = []
+//   const roofV = []
+//   const setX = new Set();
+//   const setY = new Set();
+
+//   for( let i=0; i < nFloor; i++ ){
+//     //* Podium floors
+//     let podF = new THREE.Mesh( geomF, matF );
+//     podF.applyMatrix4(matrix)
+//     podF.translateZ(-hF)
+//     podF.updateMatrixWorld()
+
+//     // floorMeshes.push(podF)
+//     scene.add(podF); //TODO: merge then addd to scene
+//     hF += depth + floorHeight
+
+//     //* Podium floors
+//     let podS = new THREE.Mesh( geomS, matS );
+//     podS.applyMatrix4(matrix)
+//     podS.translateZ(-hS)
+//     podS.updateMatrixWorld()
+
+//     // spaceMeshes.push(podS)
+//     scene.add(podS); //TODO: merge then addd to scene
+//     hS += depth + floorHeight
+
+//     //* Add podium roof
+//     if( i == nFloor - 1 ){
+//       let roof = new THREE.Mesh( geomF, matF );
+//       roof.applyMatrix4(matrix)
+//       roof.translateZ(-hF)
+//       roof.updateMatrixWorld()
+//       roof.updateMatrix()
+//       hF += depth/2
+
+//       // roof.geometry.computeBoundingBox()
+//       // let box = new THREE.Box3().setFromObject( roof );
+//       // let bBox = visualizeBBox(box, true)
+//       for ( let y=0; y<roof.geometry.index.count; y++ ){
+//         let v3 = new THREE.Vector3().fromBufferAttribute(roof.geometry.attributes.position, y);
+//         roofV.push(v3)
+//       }
+//       const m = roofV.map( v=>{
+//         if (v.x) setX.add(v.x)
+//         if (v.y) setY.add(v.y)
+
+//       })
+//       scene.add(roof); //TODO: merge then addd to scene
+//     }
+//   }
+  
+//   //* Roof points
+//   const topP = []
+//   const x = Array.from(setX)
+//   const y = Array.from(setY)
+
+//   for( let a=0; a < x.length; a++ ){
+//     for( let b=0; b < y.length; b++ ){
+//       topP.push(new THREE.Vector3(x[a], y[b], -hF ))
+//     }
+//   }
+
+//   //!<              Visualize top points              >!
+//   const geometry = new THREE.SphereGeometry( 0.1, 32, 16 ); 
+//   const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } ); 
+//   for( let r=0; r <topP.length; r++){
+//     let s = new THREE.Mesh( geometry, material ); 
+//     s.position.copy(topP[r])
+//     s.applyMatrix4(matrix)
+//     scene.add( s );
+//   }
+//   console.log("Roof: ", topP)
+
+//   topPodium = hF
+//   return {height: topPodium, topVectors: topP}
+
+// }
+// const rndInt = randomIntFromInterval(1, 4);
+// // const floors = nDivider(rndInt, 6, 2);
+// const pod = createPodium(dist, dist/2, 0.2, podiumBase.matrix , rndInt, 2)
+// console.log(`Top podium is at: ${topPodium}`)
+// console.log('Podium: ', pod)
+
+
+
+
+
+
+
